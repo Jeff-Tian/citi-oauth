1a41e08ed3f8ec8498968e3667b312c56aa4bf44
Jeff Tian <jie.tian@unicareer.com>
2019-11-04 20:33:34 +0800

    fix: countryCode to upper case

--- src/reward.ts
+++ src/reward.ts
@@ -1,41 +1,40 @@
 import axios from 'axios'
 import querystring from 'querystring'
-import CitiOAuth from "."
+import CitiOAuth from '.'
 import uuid from 'uuid/v4'
 
 export default class CitiReward {
-    citi: CitiOAuth;
-    constructor(citi: CitiOAuth) {
-        this.citi = citi;
+  citi: CitiOAuth
+
+  constructor(citi: CitiOAuth) {
+    this.citi = citi
+  }
+
+  public async createLinkCode() {}
+
+  public async getPointBalance(countryCode: string = 'US', cloakedCreditCardNumber?: string) {
+    const url = `/v1/rewards/pointBalance`
+    const qs: any = {
+      cloakedCreditCardNumber,
+      merchantCode: 'FLOWR',
+      rewardProgram: 'THANKU',
     }
 
-    public async createLinkCode() {
-
+    if (!cloakedCreditCardNumber) {
+      qs.rewardLinkCode = '998OB390B502W4G4PQIMGP8P4155378GM4SQ3ORF418134ST'
     }
 
-    public async getPointBalance(countryCode: string = 'US', cloakedCreditCardNumber?: string) {
-        const url = `/v1/rewards/pointBalance`
-        const qs: any = {
-            cloakedCreditCardNumber,
-            merchantCode: 'FLOWR',
-            rewardProgram: 'THANKU',
-        }
-
-        if (!cloakedCreditCardNumber) {
-            qs.rewardLinkCode = '998OB390B502W4G4PQIMGP8P4155378GM4SQ3ORF418134ST'
-        }
-
-        const headers = {
-            accept: 'application/json',
-            'accept-language': 'en-us',
-            authorization: `Bearer ${(await this.citi.getClientAccessToken(countryCode)).access_token}`,
-            businesscode: 'GCB',
-            countrycode: countryCode,
-            'content-type': 'application/json',
-            uuid: uuid(),
-            client_id: this.citi.appId
-        }
-
-        return await this.citi.wrap(axios.get)(url + '?' + querystring.stringify(qs), { headers })
+    const headers = {
+      accept: 'application/json',
+      'accept-language': 'en-us',
+      authorization: `Bearer ${(await this.citi.getClientAccessToken(countryCode)).access_token}`,
+      businesscode: 'GCB',
+      countrycode: countryCode.toUpperCase(),
+      'content-type': 'application/json',
+      uuid: uuid(),
+      client_id: this.citi.appId,
     }
+
+    return await this.citi.wrap(axios.get)(url + '?' + querystring.stringify(qs), { headers })
+  }
 }
