9296a6166148e18aa7caa22e604306ccd8bfb8a9
Jeff Tian <jeff.tian@outlook.com>
2019-11-17 15:42:32 +0800

    fix: Cannot read property 'data' of undefined on error

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,5 @@
+import R from 'ramda'
+
 class CitiAPIError extends Error {
   public code!: string
   public meta!: object
@@ -14,30 +16,35 @@ export const wrapper = (
   settings?: any
 ) => {
   return async (url: string, data?: any, options?: any) => {
+    if (!settings.logger) {
+      settings.logger = console
+    }
+
     try {
-      const {data: res} = await requestFunc(
+      const { data: res } = await requestFunc(
         url.startsWith('http') ? url : `${settings.endpoint}${url}`,
         data,
         options
       )
 
-      if (res.errcode) {
-        const error = new CitiAPIError(res.errmsg)
-        error.code = res.errcode
-        error.meta = {url, options}
+      return R.tap((res) => {
+        if (res.errcode) {
+          const error = new CitiAPIError(res.errmsg)
+          error.code = res.errcode
+          error.meta = { url, options }
 
-        throw error
-      }
-
-      return res
+          throw error
+        }
+      })(res)
     } catch (err) {
-      ;(settings.logger || console).error(
+      settings.logger.error(
         '碰到了错误！',
-        JSON.stringify(err.response.data)
+        err.response
       )
       err.request = {}
-      err.response = {data: err.response ? err.response.data : null}
-      ;(settings.logger || console).error(err)
+      err.response = { data: err.response ? err.response.data : null }
+      settings.logger.error(err)
+
       if (!(err instanceof CitiAPIError)) {
         err.name = 'CitiAPI' + err.name
       }
